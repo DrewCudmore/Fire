19f8ef5195527fc07964bd1d39b336e12730f92e
maryhenthorn <159013777+maryhenthorn@users.noreply.github.com>
2024-03-19 17:05:21 +0100

    Draft Lower Text Panel
    The start of a space for text to go at the bottom of the screen

---
+++ Woods/Assets/Scripts/TextPanel.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPanel : MonoBehaviour
+{
+    [SerializeField] private GameObject textPanel;
+    [SerializeField] private bool isPanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPanel = !isPanel;
+        }
+
+        if (isPanel)
+        {
+            ActivatePanel();
+        }
+        else
+        {
+            DeactivatePanel();
+        }
+    }
+
+    void ActivatePanel()
+    {
+        textPanel.SetActive(true);
+    }
+
+    void DeactivatePanel()
+    {
+        textPanel.SetActive(false);
+    }
+
+}
