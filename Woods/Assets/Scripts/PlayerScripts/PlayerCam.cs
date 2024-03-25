25d3b8d24d5d940b5c2849b6ee8671e2998a6f6d
aidengage <aidenfgage@gmail.com>
2024-03-25 22:13:57 +0100

    basic rework of movement system
    need to fix camera turning around when game start

--- Woods/Assets/Scripts/PlayerScripts/PlayerCam.cs
+++ Woods/Assets/Scripts/PlayerScripts/PlayerCam.cs
@@ -3,70 +3,84 @@ using UnityEngine;
 public class PlayerCam : MonoBehaviour
 {
     // view bob
-    public Vector3 restPosition;
-    public float transitionSpeed = 20f;
-    public float bobSpeed = 4.8f;
-    public float bobAmount = 0.05f;
+    //public Vector3 restPosition;
+    //public float transitionSpeed = 20f;
+    //public float bobSpeed = 4.8f;
+    //public float bobAmount = 0.05f;
 
-    float timer = Mathf.PI / 2;
-    Vector3 camPos;
+    //float timer = Mathf.PI / 2;
+    //Vector3 camPos;
 
-    public float mouseSensitivity = 100f;
-    public Transform playerBody;
+    //public float mouseSensitivity = 100f;
+    public float sensX;
+    public float sensY;
 
-    float xRotation = 0f;
+    public Transform orientation;
 
-    void Awake() { camPos = transform.localPosition; }
-    void Start() { Cursor.lockState = CursorLockMode.Locked; }
+    float xRotation;
+    float yRotation;
+
+
+    //void Awake()
+    //{
+    //    camPos = transform.localPosition;
+    //}
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        //orientation.Rotate(Vector3.up * mouseX);
 
-        HeadBob();
+        //HeadBob();
     }
 
-    void HeadBob()
-    {
-        if (Input.GetAxisRaw("Horizontal") != 0 ||
-            Input.GetAxisRaw("Vertical") != 0)
-        {
-            timer += bobSpeed * Time.deltaTime;
+    //void HeadBob()
+    //{
+    //    if (Input.GetAxisRaw("Horizontal") != 0 ||
+    //        Input.GetAxisRaw("Vertical") != 0)
+    //    {
+    //        timer += bobSpeed * Time.deltaTime;
 
-            Vector3 newPos = new Vector3(
-                Mathf.Cos(timer) * bobAmount,
-                restPosition.y + Mathf.Abs((Mathf.Sin(timer) * bobAmount)),
-                restPosition.z);
-            camPos = newPos;
-        }
-        else
-        {
-            timer = Mathf.PI / 2;
+    //        Vector3 newPos = new Vector3(
+    //            Mathf.Cos(timer) * bobAmount,
+    //            restPosition.y + Mathf.Abs((Mathf.Sin(timer) * bobAmount)),
+    //            restPosition.z);
+    //        camPos = newPos;
+    //    }
+    //    else
+    //    {
+    //        timer = Mathf.PI / 2;
 
-            Vector3 newPos =
-                new Vector3(Mathf.Lerp(camPos.x, restPosition.x,
-                                       transitionSpeed * Time.deltaTime),
-                            Mathf.Lerp(camPos.y, restPosition.y,
-                                       transitionSpeed * Time.deltaTime),
-                            Mathf.Lerp(camPos.z, restPosition.z,
-                                       transitionSpeed * Time.deltaTime));
-            camPos = newPos;
-            // Vector3 newPos = new Vector3(Mathf.Lerp(camPos.x, restPosition.x,
-            // transitionSpeed * Time.deltaTime), Mathf.Lerp((camPos.y,
-            // restPosition.y, transitionSpeed * Time.deltaTime), Mathf.Lerp(camPos.z,
-            // restPosition.z, transitionSpeed * Time.deltaTime)));
-        }
+    //        Vector3 newPos =
+    //            new Vector3(Mathf.Lerp(camPos.x, restPosition.x,
+    //                                   transitionSpeed * Time.deltaTime),
+    //                        Mathf.Lerp(camPos.y, restPosition.y,
+    //                                   transitionSpeed * Time.deltaTime),
+    //                        Mathf.Lerp(camPos.z, restPosition.z,
+    //                                   transitionSpeed * Time.deltaTime));
+    //        camPos = newPos;
+    //        // Vector3 newPos = new Vector3(Mathf.Lerp(camPos.x, restPosition.x,
+    //        // transitionSpeed * Time.deltaTime), Mathf.Lerp((camPos.y,
+    //        // restPosition.y, transitionSpeed * Time.deltaTime), Mathf.Lerp(camPos.z,
+    //        // restPosition.z, transitionSpeed * Time.deltaTime)));
+    //    }
 
-        if (timer > Mathf.PI * 2)
-        {
-            timer = 0;
-        }
-    }
+    //    if (timer > Mathf.PI * 2)
+    //    {
+    //        timer = 0;
+    //    }
+    //}
 }
--- Woods/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Woods/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -2,40 +2,106 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
-    public float speed = 12f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
+    [Header("Movement")]
+    public float moveSpeed;
 
-    Vector3 velocity;
-    bool canMove = true; // Flag to control player movement
+    public float groundDrag;
 
-    void Update()
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask GroundLayer;
+    bool grounded;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+    public bool canMove = true;
+
+    private void Start()
     {
-        if (!canMove)
-            return; // Don't execute movement if movement is disabled
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
 
-        bool isGrounded = controller.isGrounded;
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
 
-        if (isGrounded && velocity.y < 0)
+    private void Update()
+    {
+        // checks ground every frame
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, GroundLayer);
+
+        MyInput();
+
+        // slows player down while on ground (ground drag)
+        // we can probably use a material if we want to
+        if (grounded)
         {
-            velocity.y = -2f;
+            rb.drag = groundDrag;
         }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = this.transform.right * x + this.transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        else
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            rb.drag = 0;
         }
     }
+
+    void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    void MovePlayer()
+    {
+        // calculates movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+
+    // old
+    //public CharacterController controller;
+    //public float speed = 12f;
+    //public float gravity = -9.81f;
+    //public float jumpHeight = 3f;
+
+    //Vector3 velocity;
+    //bool canMove = true; // Flag to control player movement
+
+    //void Update()
+    //{
+    //    if (!canMove)
+    //        return; // Don't execute movement if movement is disabled
+
+    //    bool isGrounded = controller.isGrounded;
+
+    //    if (isGrounded && velocity.y < 0)
+    //    {
+    //        velocity.y = -2f;
+    //    }
+
+    //    float x = Input.GetAxis("Horizontal");
+    //    float z = Input.GetAxis("Vertical");
+
+    //    Vector3 move = this.transform.right * x + this.transform.forward * z;
+    //    controller.Move(move * speed * Time.deltaTime);
+
+    //    velocity.y += gravity * Time.deltaTime;
+    //    controller.Move(velocity * Time.deltaTime);
+
+    //    if (isGrounded && Input.GetButtonDown("Jump"))
+    //    {
+    //        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    //    }
+    //}
 
     public void enableMovement()
     {
