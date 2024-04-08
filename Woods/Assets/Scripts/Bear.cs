5497647cda800e2d4dd0e4dd7a5c03bf0a1b0866
aidengage <aidenfgage@gmail.com>
2024-04-08 22:28:28 +0200

    animation reactions to player based on triggers

--- Woods/Assets/Scripts/Bear.cs
+++ Woods/Assets/Scripts/Bear.cs
@@ -7,11 +7,19 @@ public class Bear : MonoBehaviour
     public Rigidbody rb;
     Animator ani;
 
+    public BoxCollider startWalking;
+    public BoxCollider attackPlayer;
+
+    public int triggerCounter = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         ani = GetComponent<Animator>();
+
+        startWalking = GetComponent<BoxCollider>();
+        attackPlayer = GetComponent<BoxCollider>();
 
         ani.SetBool("Walking", false);
         ani.SetBool("Running", false);
@@ -34,8 +42,39 @@ public class Bear : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            ani.SetBool("Walking", true);
-            ani.SetBool("Sleeping", false);
+            triggerCounter++;
+            Debug.Log("current trigger value on enter " + triggerCounter);
+            if (triggerCounter == 1)
+            {
+                Debug.Log("bear started walking");
+                ani.SetBool("Walking", true);
+                ani.SetBool("Sleeping", false);
+            }
+            else if (triggerCounter == 2)
+            {
+                Debug.Log("bear attacked");
+                ani.SetBool("Attack", true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (triggerCounter == 2)
+            {
+                Debug.Log("bear stop attack");
+                ani.SetBool("Attack", false);
+            }
+            else if (triggerCounter == 1)
+            {
+                Debug.Log("bear stop walking");
+                ani.SetBool("Walking", false);
+                ani.SetBool("Sleeping", true);
+            }
+            triggerCounter--;
+            Debug.Log("current trigger value on exit " + triggerCounter);
         }
     }
 }
