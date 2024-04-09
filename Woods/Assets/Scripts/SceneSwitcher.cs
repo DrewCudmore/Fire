606be37f92c1af3844b45542651e30ea1c64b113
DrewCudmore <91638517+DrewCudmore@users.noreply.github.com>
2024-04-09 11:02:01 +0200

    ADD CAR SWITCH TO MAIN AHHHHHH

---
+++ Woods/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public string sceneToSwitchTo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Car")
+        {
+            SceneManager.LoadScene(sceneToSwitchTo);
+        }
+    }
+}
