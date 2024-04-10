6e4e21762c6ff6f4d42445a1a0d9fabbe9eb77a9
Jessica W <102379487+jkwarren@users.noreply.github.com>
2024-04-10 21:38:39 +0200

    text popup for lantern battery

--- Woods/Assets/Scripts/Lantern.cs
+++ Woods/Assets/Scripts/Lantern.cs
@@ -9,6 +9,9 @@ public class Lantern : MonoBehaviour
     private float originalIntensity;// = myLight.intensity;
     private float originalRange;
     private Color originalColor;
+    private float halfIntensity;
+    private float nearlyOutIntensity;
+    public bool textIntensity = false;
 
     private float deincrementLight = .0009f;
 
@@ -19,17 +22,22 @@ public class Lantern : MonoBehaviour
     public bool resetLantern = false;
 
     private GameManager gameManager;
+    private TextPanel textPanel;
     //private 
-    
+
     void Start()
     {
         myLight = GetComponent<Light>();
+        textPanel = FindObjectOfType<TextPanel>(); 
         gameManager = GameManager.Instance;
         originalIntensity = myLight.intensity;
         originalRange = myLight.range;
         originalColor = myLight.color;
-        
-    }
+
+        halfIntensity = originalIntensity / 2;
+        nearlyOutIntensity = 1f;
+
+}
 
     // Update is called once per frame
     void FixedUpdate()
@@ -42,6 +50,17 @@ public class Lantern : MonoBehaviour
             colorLight += colorChangeRate * Time.deltaTime;
             myLight.color = new Color(myLight.color.r, colorLight, myLight.color.b, myLight.color.a);
             checkBurnout();
+            if((myLight.intensity < halfIntensity) & (myLight.intensity > halfIntensity - .5 ) & (textIntensity == false))
+            {
+                textIntensity = true;
+                LanternBattery(halfIntensity);
+            }
+
+            else if ((myLight.intensity < nearlyOutIntensity) & (myLight.intensity > nearlyOutIntensity - .5) & (textIntensity == true))
+            {
+                textIntensity = false;
+                LanternBattery(nearlyOutIntensity);
+            }
 
         }
         if (resetLantern)
@@ -56,6 +75,20 @@ public class Lantern : MonoBehaviour
         myLight.intensity = originalIntensity;
         myLight.range = originalRange;
         myLight.color = originalColor;
+        textIntensity = false;
+    }
+    void LanternBattery(float intensity)
+    {
+        
+        if (intensity == halfIntensity)
+        {
+            textPanel.DisplayText("Looks like my light is about halfway out, I should refill it at a campfire.");
+        }
+
+        if (intensity == nearlyOutIntensity)
+        {
+            textPanel.DisplayText("I'm almost out of light, I should refill at a campfire.");
+        }
     }
     void checkBurnout()
     {
