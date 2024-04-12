6392e57f9bd55f1412563601d0a01f0d2b82a007
DrewCudmore <91638517+DrewCudmore@users.noreply.github.com>
2024-04-12 12:08:12 +0200

    Add Start Game Button

---
+++ Woods/Assets/Scripts/MenuScripts/StartMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject startMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
