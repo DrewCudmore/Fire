2accba68da6421e59ba63111793456850d810b49
DrewCudmore <91638517+DrewCudmore@users.noreply.github.com>
2024-04-23 11:00:19 +0200

    Add moveable stalagtite

---
+++ Woods/Assets/Scripts/MoveableObstacle.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObstacle : MonoBehaviour
+{
+
+    public GameObject blueTorch;
+    public GameObject redTorch;
+    public GameObject greenTorch;
+
+    public float moveSpeed = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CheckLit())
+        {
+            MoveAway();
+        }
+    }
+
+    private bool CheckLit()
+    {
+        return blueTorch.GetComponent<IInteractable>().CanInteract && redTorch.GetComponent<IInteractable>().CanInteract && greenTorch.GetComponent<IInteractable>().CanInteract;
+    }
+
+    private void MoveAway()
+    {
+        transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        Destroy(gameObject, 10f);
+    }
+
+
+
+}
