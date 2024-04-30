1f8509ab926a64eab468ec8e4bfad510132df8ea
Jessica W <102379487+jkwarren@users.noreply.github.com>
2024-04-30 10:43:50 +0200

    berries

--- Woods/Assets/Scripts/Managers/GameManager.cs
+++ Woods/Assets/Scripts/Managers/GameManager.cs
@@ -24,7 +24,6 @@ public class GameManager : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         playerMovement = player.GetComponent<PlayerMovement>();
         textPanel = FindObjectOfType<TextPanel>();
-        textPanel.DisplayText("Use WASD and E to interact");
 
         FadeInScreen(fadeDuration);
     }
