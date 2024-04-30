e933f5c46a241fa48947116769e88b014e784f1d
DrewCudmore <91638517+DrewCudmore@users.noreply.github.com>
2024-04-30 10:56:39 +0200

    Fix start menu not clicking

--- Woods/Assets/Scripts/MenuScripts/StartMenu.cs
+++ Woods/Assets/Scripts/MenuScripts/StartMenu.cs
@@ -12,7 +12,13 @@ public class StartMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnlockCursor();
+    }
+
+    public void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
