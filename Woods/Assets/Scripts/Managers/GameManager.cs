4abc322bceedfd360063f4c71c52ed392240d158
DrewCudmore <91638517+DrewCudmore@users.noreply.github.com>
2024-05-01 22:01:19 +0200

    Fix No Checkpoint Glitch

--- Woods/Assets/Scripts/Managers/GameManager.cs
+++ Woods/Assets/Scripts/Managers/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     private GameObject player;
     PlayerMovement playerMovement;
-    private Transform lastCheckpoint;
+    public Transform lastCheckpoint;
 
     private TextPanel textPanel;
 
