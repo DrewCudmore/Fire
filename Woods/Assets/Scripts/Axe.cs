fa1ad6719d4966dafd920d73ae8155c679b46df5
DrewCudmore <91638517+DrewCudmore@users.noreply.github.com>
2024-05-05 15:57:14 +0200

    Add Axe Pickup Audio

--- Woods/Assets/Scripts/Axe.cs
+++ Woods/Assets/Scripts/Axe.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Axe : MonoBehaviour, IInteractable
 {
     public bool CanInteract { get; set; } = true;
+    public AudioSource audioSource;
+    public Renderer rend;
+
+    private void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+    }
 
     public Item Interact()
     {
-        Destroy(this.gameObject);
+        audioSource.Play();
+        rend.enabled = false;
+        Destroy(this.gameObject, 2f);
 
         return Item.Axe;
     }
